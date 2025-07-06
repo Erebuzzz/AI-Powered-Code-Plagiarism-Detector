fc2ba642a4c1f06c6e0cc02e6a772b596f2b1f44
Ayra <ayra.wrk@gmail.com>
2025-07-07 05:23:57 +0530

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,16 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
   env: {
     BACKEND_URL: process.env.BACKEND_URL || 'http://localhost:5001',
-  },
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: `${process.env.BACKEND_URL || 'http://localhost:5001'}/api/:path*`,
-      },
-    ];
   },
 };
 
